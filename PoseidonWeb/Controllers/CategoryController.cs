6c737593fc3c60d8eaa6d6a35ab04225577d1a04
Teemu Harju <teemu.harju1997@outlook.com>
2022-08-07 15:21:17 +0300

    Create Category View added

--- PoseidonWeb/Controllers/CategoryController.cs
+++ PoseidonWeb/Controllers/CategoryController.cs
@@ -18,5 +18,11 @@ namespace PoseidonWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
