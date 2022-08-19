36382303a3444216555a6421b6b3f19c94697489
Teemu Harju <teemu.j.harju@tuni.fi>
2022-08-19 22:10:59 +0300

    Create and Edit Category done.

--- PoseidonWeb/Controllers/CategoryController.cs
+++ PoseidonWeb/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ namespace PoseidonWeb.Controllers
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext db)
+        public CategoryController( ApplicationDbContext db )
         {
             _db = db;
         }
@@ -16,13 +16,67 @@ namespace PoseidonWeb.Controllers
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
-            return View(objCategoryList);
+            return View( objCategoryList );
         }
 
         // GET
         public IActionResult Create()
         {
             return View();
+        }
+
+        // POST
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create( Category obj )
+        {
+            if ( obj.Name == obj.DisplayOrder.ToString() )
+            {
+                ModelState.TryAddModelError( "CustomError", "The DisplayOrder cannot exactly match the Name" ); 
+            }
+            if( ModelState.IsValid )
+            {
+                _db.Categories.Add( obj );
+                _db.SaveChanges();
+                return RedirectToAction( "Index" );
+            }
+            return View( obj );
+        }
+
+        // GET
+        public IActionResult Edit( int ? id )
+        {
+            if ( id == null || id == 0 )
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find( id );
+
+            if( categoryFromDb == null )
+            {
+                return NotFound();
+            }
+
+            return View( categoryFromDb );
+        }
+
+        // POST
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit( Category obj )
+        {
+            if ( obj.Name == obj.DisplayOrder.ToString() )
+            {
+                ModelState.TryAddModelError( "CustomError", "The DisplayOrder cannot exactly match the Name" );
+            }
+            if ( ModelState.IsValid )
+            {
+                _db.Categories.Update( obj );
+                _db.SaveChanges();
+                return RedirectToAction( "Index" );
+            }
+            return View( obj );
         }
     }
 }
--- PoseidonWeb/Models/Category.cs
+++ PoseidonWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PoseidonWeb.Models
 {
@@ -6,9 +7,14 @@ namespace PoseidonWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
+        [DisplayName( "Display Order" )]
+        [Range( 1,100, ErrorMessage = "Display Oder must be between 1 and 100 only!!" )]
         public int DisplayOrder { get; set; }
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
